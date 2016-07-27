6caa96f03fdf953f6e1dcb6d7fcae821d4afc8df
Hugh <hugh_gleaves@hotmail.com>
2016-07-27 11:15:51 -0700

    Added a simple solution to a question found on MSDN forum.

--- FSharpTagStripper/FSharpTagStripper/Program.fs
+++ FSharpTagStripper/FSharpTagStripper/Program.fs
@@ -6,6 +6,8 @@ open System
 [<EntryPoint>]
 let main argv = 
     
+    // Motivated by an interest in simplifying an earlier C# FSM design.
+
     let strip_tags input = 
         let rec get_next_character (flag, text) = 
             match flag with
@@ -18,5 +20,16 @@ let main argv =
         Seq.unfold get_next_character (true, input) |> Seq.toArray |> String 
 
     let result = strip_tags "This text is HTML with an embedded<img> image tag."
+
+    // Motivated by: https://social.msdn.microsoft.com/Forums/vstudio/en-US/11be467d-9153-4318-8d79-f300ce9ccf39/f-get-each-element-from-the-list-in-seqchoose?forum=fsharpgeneral
+
+    let lines = [| ("A 1 100"); ("B 2 200"); ("C 3 300") |]
+
+    let tupelize input =
+        let create_tuple (text:string) =
+            (Int32.Parse (text.Split ' ').[1], Int32.Parse (text.Split ' ').[2])
+        Array.map create_tuple input
+    
+    let tuples = tupelize lines 
 
     0 // return an integer exit code

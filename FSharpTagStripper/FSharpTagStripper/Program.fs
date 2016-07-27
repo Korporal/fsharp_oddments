d80ffa45d7a8dc68aa08c923538af767533e34fb
Hugh <hugh_gleaves@hotmail.com>
2016-07-27 16:50:57 -0700

    Remove unneeded match entry.

--- FSharpTagStripper/FSharpTagStripper/Program.fs
+++ FSharpTagStripper/FSharpTagStripper/Program.fs
@@ -52,7 +52,6 @@ let main argv =
             let skip_last (list:list<'a>)  = List.rev list |> skip_first |> List.rev
             let append_to lista listb = List.append lista listb
             match n, list with
-            | _, _  when n = 0 -> list
             | _, _  when n % list.Length = 0 -> list
             | _, _  when n > 0 -> (list |> take_first |> append_to (list |> skip_first)) |> rotate_list (n - 1) 
             | _, _  when n < 0 -> (list |> skip_last  |> append_to (list |> take_last))  |> rotate_list (n + 1) 

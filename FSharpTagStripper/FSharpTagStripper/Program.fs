cb3ab49e61c5e8899b07f64370d1e8ee7c20ca26
Hugh <hugh_gleaves@hotmail.com>
2016-07-27 16:45:12 -0700

    Added reasonable implementation of a list rotate operation as discusssed on the OCaml site.

--- FSharpTagStripper/FSharpTagStripper/Program.fs
+++ FSharpTagStripper/FSharpTagStripper/Program.fs
@@ -41,6 +41,24 @@ let main argv =
         | true  -> Seq.append (Seq.take (n-1) sequence) (drop_every n (Seq.skip n sequence))
            
     let ss = drop_every 3 "123456789" |> Seq.toList   // list just makes viewing results in debug easier.
-            
+    
+    // Rotate a list n places left or right: http://ocaml.org/learn/tutorials/99problems.html
+    
+    let rotate n (list:list<'a>) =  
+        let rec rotate_list n (list:list<'a>) =
+            let take_first (list:list<'a>) = list |> List.take 1
+            let skip_first (list:list<'a>) = list |> List.skip 1
+            let take_last (list:list<'a>)  = List.rev list |> take_first
+            let skip_last (list:list<'a>)  = List.rev list |> skip_first |> List.rev
+            let append_to lista listb = List.append lista listb
+            match n, list with
+            | _, _  when n = 0 -> list
+            | _, _  when n % list.Length = 0 -> list
+            | _, _  when n > 0 -> (list |> take_first |> append_to (list |> skip_first)) |> rotate_list (n-1) 
+            | _, _  when n < 0 -> (list |> skip_last  |> append_to (list |> take_last))  |> rotate_list (n+1) 
+        rotate_list n list
+
+    let rotated = rotate -2 ("abcdefghij" |> Seq.toList)
+
 
     0 // return an integer exit code

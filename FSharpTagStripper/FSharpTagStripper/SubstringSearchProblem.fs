da77ba47e939dbe2856bfe01238af760f2679b33
Hugh <hugh_gleaves@hotmail.com>
2016-08-16 00:01:40 -0700

    Minor renames and added the 'generate_table' function to gen a complete partial match table in the form of an array, for any given word.

--- FSharpTagStripper/FSharpTagStripper/SubstringSearchProblem.fs
+++ FSharpTagStripper/FSharpTagStripper/SubstringSearchProblem.fs
@@ -6,13 +6,13 @@ let solve_problem() =
     
     // Returns a sequence of proper suffixes of the input string in descending order by length.
     let get_suffixes (text:string) =
-        let J = text.Length - 1
-        seq {for I = 1 to J do yield text.[I .. J]} 
+        let L = text.Length - 1
+        seq {for I = 1 to L do yield text.[I .. L]} 
     
     // Returns a sequence of proper prefixes of the input string in descending order by length.
     let get_prefixes (text:string) =
-        let J = text.Length - 2
-        seq {for I = J downto 0 do yield text.[0 .. I]}
+        let L = text.Length - 2
+        seq {for I = L downto 0 do yield text.[0 .. I]}
 
     // Given a sequence of matching suffix/prefix pairs, returns the length of the longest.
     let longest_match pairs =
@@ -25,5 +25,8 @@ let solve_problem() =
         let suffixes = get_suffixes word
         let prefixes = get_prefixes word
         Seq.zip suffixes prefixes |> Seq.filter (fun e -> (fst e) = (snd e)) |> longest_match
-    0
 
+    let generate_table (word:string) =
+        let L = word.Length - 1
+        seq {for I = 0 to L do yield get_partial_match_value word.[0 .. I]} |> Seq.toArray
+    0

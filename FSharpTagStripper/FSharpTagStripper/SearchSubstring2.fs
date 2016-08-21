642d69ce300880c110a801f329062bf91c2c0a15
Hugh <hugh_gleaves@hotmail.com>
2016-08-21 11:18:43 -0700

    Added a recurisve function to generate table based on logic described here (in C): https://en.wikibooks.org/wiki/Algorithm_Implementation/String_searching/Knuth-Morris-Pratt_pattern_matcher#C_and_Java

--- FSharpTagStripper/FSharpTagStripper/SearchSubstring2.fs
+++ FSharpTagStripper/FSharpTagStripper/SearchSubstring2.fs
@@ -3,10 +3,10 @@
 open System
 
 let get_proper_prefixes L (word:seq<char>) =
-    seq {for I = L-1 downto 1 do yield Seq.take I word} |> Seq.toList
+    seq {for I = L-1 downto 1 do yield Seq.take I word} 
 
 let get_proper_suffixes L (word:seq<char>) =
-    seq {for I = 1 to L-1 do yield Seq.skip I word} |> Seq.toList
+    seq {for I = 1 to L-1 do yield Seq.skip I word} 
 
 let equal seq1 seq2 =
     Seq.compareWith (fun x y -> if x = y then 0 else 1) seq1 seq2
@@ -25,3 +25,28 @@ let get_partial_match_value L (word:seq<char>) =
     | (true, true) -> 0
     | (_,_) -> Seq.zip suffixes prefixes |> first_match
     
+let generate_table (word:string) =
+    let L = word.Length 
+    seq {for I = 1 to L do yield Seq.take I word |> get_partial_match_value I} |> Seq.toArray
+
+let compute_table (word:string) =
+    let prev_longest = 0
+    let index = 1
+    let table = Array.create word.Length 0
+    
+    let rec inner_computation index prev (table:int[]) =
+        if index >= word.Length then
+           table
+        else
+            if word.[index] = word.[prev] then
+               table.[index] <- prev+1
+               inner_computation (index+1) (prev+1) table
+            else
+                if prev = 0 then
+                   table.[index] <- 0
+                   inner_computation (index+1) prev table
+                else
+                   inner_computation index table.[prev-1] table
+    
+    inner_computation index prev_longest table
+
